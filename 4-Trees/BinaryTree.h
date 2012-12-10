e5f201247b876888aab104dab3ba4a861a2f9fd1
Moshe Berman <moshberm@gmail.com>
2012-12-10 10:17:06 -0500

    Added traversal methods.

--- 4-Trees/BinaryTree.h
+++ 4-Trees/BinaryTree.h
@@ -9,11 +9,48 @@
 #ifndef __Trees_BinaryTree_h
 #define __Trees_BinaryTree_h
 
+//
+//  Define a Node and a pointer type
+//
+
 struct Node {
     Node *left, *right;
     int value;
 };
 
 typedef Node* NodePointer;
+
+//
+//  Some binary tree traversal methods
+//
+
+void preTrav(NodePointer node){
+    preTrav(node->left);
+    preTrav(node->right);
+    
+    //
+    //  TODO: some action goes here
+    //
+}
+
+void inTrav(NodePointer node){
+    inTrav(node->left);
+    
+    //
+    //  TODO: some action goes here
+    //
+    
+    inTrav(node->right);
+}
+
+void postTrav(NodePointer node){
+    
+    //
+    //  TODO: Some action goes here
+    //
+    
+    postTrav(node->left);
+    postTrav(node->right);
+}
 
 #endif
