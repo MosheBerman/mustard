0a42ea164663d818a3306bd3cb6e36fbb4fa15a9
Moshe Berman <moshberm@gmail.com>
2012-12-19 17:44:33 -0500

    Created a Set file.

---
+++ 4-Trees/Set.h
@@ -0,0 +1,25 @@
+//
+//  Set.h
+//  4-Trees
+//
+//  Created by Moshe Berman on 12/19/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#ifndef __Trees_Set_h
+#define __Trees_Set_h
+
+#include <vector>
+
+class Instruction {
+    int number;
+    string instruction;
+};
+
+class Set {
+public:
+    vector<int> numbers;
+    vector<Instruction> Instructions;
+};
+
+#endif
